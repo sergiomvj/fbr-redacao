ca943a5ff6404561c52b5e58bf36dbb9da5aae6f
Sergio Castro <sergiomvj@gmail.com>
2026-03-03 16:07:30 -0300

    feat: Adiciona script para testar o endpoint de login.

--- frontend/test_login.js
+++ frontend/test_login.js
@@ -17,17 +17,17 @@ const options = {
 };
 
 const req = http.request(options, (res) => {
-    console.log(\`STATUS: \${res.statusCode}\`);
-    console.log(\`HEADERS: \${JSON.stringify(res.headers, null, 2)}\`);
-    
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers, null, 2)}`);
+
     res.setEncoding('utf8');
     res.on('data', (chunk) => {
-        console.log(\`BODY: \${chunk}\`);
+        console.log(`BODY: ${chunk}`);
     });
 });
 
 req.on('error', (e) => {
-    console.error(\`problem with request: \${e.message}\`);
+    console.error(`problem with request: ${e.message}`);
 });
 
 req.write(data);
